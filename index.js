7defe23c6a7a2d6439a1ce593859425fc75fbc6f
CrazyDog1811 <evgeshatertyshnyi@mail.ru>
2022-01-26 14:24:12 +0300

    feat: add function changeImage

--- index.js
+++ index.js
@@ -1,44 +1,69 @@
 const birdsBlock = document.querySelector('ul');
 const birds = birdsBlock.querySelectorAll('.bird');
+const img = document.querySelector('img');
+const btnPlay = document.querySelector('.btn-play');
+
 const audio = new Audio();
+audio.src = './assets/audio/solovey.mp3';
 let isPlay = false;
 
 function playAudio(ev) {
 
     const targetClass = ev.target.classList;
 
-    if(!targetClass.contains('checked') && isPlay) {
-        audio.pause();
-        isPlay = false;
-    }
+    if (targetClass.contains('bird')) {
 
-    checkBtn(ev);
+        btnPlay.classList.toggle('paused');
 
-    if (ev.target.classList.contains('bird')) {
+        changeBtn(ev);
+        changeImg(ev);
 
         const birdName = ev.target.dataset.birdName;
         audio.src = `./assets/audio/${birdName}.mp3`;
         audio.currentTime = 0;
-        if (!isPlay) {
-            audio.play();
-            isPlay = true;
-        } else {
-            audio.pause();
-            isPlay = false;
-        }
+        pausePlay();
     }
 }
 
-const checkBtn = (ev) => {
-    const check = ev.target;
-    if (check.classList.contains('checked')) {
-        check.classList.remove('checked');
+const pausePlay = () => {
+    if (!isPlay) {
+        audio.play();
+        isPlay = true;
+    } else {
+        audio.pause();
+        isPlay = false;
+    }
+}
+
+const changeBtn = (ev) => {
+    const checkClass = ev.target.classList;
+
+    if(!checkClass.contains('checked') && isPlay) {
+        audio.pause();
+        isPlay = false;
+    }
+
+    if (checkClass.contains('checked')) {
+        checkClass.remove('checked');
     } else {
         birds.forEach((bird) => {
             bird.classList.remove('checked');
         });
-        check.classList.add('checked');
+        checkClass.add('checked');
     }
 }
 
+const changeImg = (ev) => {
+    const birdImg = ev.target.dataset.birdName;
+  img.src = `./assets/img/${birdImg}.jpg`;
+}
+
+const mainPlayBtn = () => {
+  btnPlay.classList.toggle('paused');
+  pausePlay();
+}
+
+
+
 birdsBlock.addEventListener('click', playAudio);
+btnPlay.addEventListener('click', mainPlayBtn);
